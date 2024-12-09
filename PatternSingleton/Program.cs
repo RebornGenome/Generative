069b53053c4021901a5540e1ffc88f46e05188ab
Максим <150604137+RebornGenome@users.noreply.github.com>
2024-12-09 23:37:59 +0200

    Add files via upload

---
+++ PatternSingleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using PatternAbstractFactory.Factories;
+using PatternAbstractFactory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Выберите Тип создаваемой Мебели: 1 - Хай-Тек, 2 - Лофт, 3 - Классик");
+        string choice = Console.ReadLine().Trim();
+        IFurnitureFactory factory;
+        switch (choice)
+        {
+            case "1":
+                factory = HighTechFurnitureFactory.Instance;
+                break;
+            case "2":
+                factory = LoftFurnitureFactory.Instance;
+                break;
+            case "3":
+                factory = ClassicFurnitureFactory.Instance;
+                break;
+            default:
+                Console.WriteLine("Вы ввели НЕдопустимое Значение!!! Повторите попытку.");
+                return;
+        }
+        Client client = new Client(factory);
+        client.CreateFurniture();
+    }
+}
