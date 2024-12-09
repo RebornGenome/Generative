81848f4517b7b2f431aad04921dc68706e41c951
Максим <150604137+RebornGenome@users.noreply.github.com>
2024-12-09 23:49:19 +0200

    Add files via upload

---
+++ PatternPrototype/FurnitureForPrototypeLibrary/Furniture/ITable.cs
@@ -0,0 +1,14 @@
+﻿using FurnitureForPrototypeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternAbstractFactory.Furniture
+{
+    public interface ITable : IPrototype<ITable>
+    {
+        void Create();
+    }
+}
